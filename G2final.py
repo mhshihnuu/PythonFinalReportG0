f67b7978ed41a3600f939c2ce7483a480917a00f
mhshihnuu <mhshih@nuu.edu.tw>
2025-12-12 11:31:44 +0800

    Define G2function to calculate nearest location

---
+++ G2final.py
@@ -0,0 +1,5 @@
+def G2function(current_location=[latitude, longitude]):
+  location1 = [latitude1, longitude1]
+  distance = (latitude - latitude1)**2 + (longitude - longitude1)**2
+  ...
+  return nearest_location
